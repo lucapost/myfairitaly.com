236da04acce08fae61f306e1e23f51ebf2854789
Luca Postregna <luca.postregna@gmail.com>
2011-07-29 16:05:14 +0200

    update generare for gallery

--- images/gallery/generate.sh
+++ images/gallery/generate.sh
@@ -1,14 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ -e $1/data.txt ]; then
+	echo "$1/data.txt"
+else 
+	echo "$1/data.txt non è presente nella directory specificata"
+	exit 0
+fi
 
 outdir=`echo $1 |sed -e 's/origin\///'`
 rm -rf $outdir 
-mkdir $outdir
+mkdir $outdir tmp
 
-mkdir tmp
-cp $1/*.jpg tmp/
+sed '/^$/d' $1/data.txt > tmp/data.txt
 
-for i in `ls tmp/*.jpg` ; do
-	convert $i tmp/tmp.png
+exec 3<&0
+exec 0<tmp/data.txt
+
+while read line; do
+	
+	name=`echo $line | cut -d: -f1`
+	altit=`echo $line | cut -d: -f2`
+	titleit=`echo $line | cut -d: -f3`
+	alten=`echo $line | cut -d: -f4`
+	titleen=`echo $line | cut -d: -f5`
+
+	if [ -e $1/$name ]; then
+		echo "$1/$name"
+	else 
+		echo "$1/$name non è presente, controlla la presenza del file o lo struttura di data.txt"
+		exit 0
+	fi
+
+	convert $1/$name tmp/tmp.png
 	composite -gravity SouthEast origin/wm.png tmp/tmp.png tmp/tmp.png
 	convert tmp/tmp.png \
 		\( +clone  -alpha extract \
@@ -17,23 +40,16 @@ for i in `ls tmp/*.jpg` ; do
 		\( +clone -flop \) -compose Multiply -composite \
 		\) -alpha off -compose CopyOpacity -composite tmp/tmp.png
 	composite -background white -gravity center tmp/tmp.png origin/background.png tmp/tmp.png
-	outname=`echo $i |sed -e 's/tmp\///'`
-	convert tmp/tmp.png $outdir/$outname
-	echo $outdir/$outname
+	convert tmp/tmp.png $outdir/$name
+
+	echo "<img src=\"/images/gallery/$outdir/$name\" alt=\"$altit\" title=\"$titleit\" />"  >> tmp/it.txt
+	echo "<img src=\"/images/gallery/$outdir/$name\" alt=\"$alten\" title=\"$titleen\" />"  >> tmp/en.txt
 done
 
-echo  " " 
-sed '/^$/d' $1/it.txt > tmp/data.txt
-exec 3<&0
-exec 0<tmp/data.txt
-while read line
-do
-	# use $line variable to process line
-	name=`echo $line | cut -d: -f1`
-	alt=`echo $line | cut -d: -f2`
-	title=`echo $line | cut -d: -f3`
-	echo "<img src=\"/images/gallery/$outdir/$name\" alt=\"$alt\" title=\"$title\" />"
-done
 exec 0<&3
+
+cat tmp/it.txt
+echo ""
+cat tmp/en.txt
 
 rm -rf tmp/
