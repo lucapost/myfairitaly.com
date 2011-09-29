9a3e66e5d7c62244584ae93b648351445331ad1f
Luca Postregna <luca.postregna@gmail.com>
2011-09-29 21:31:47 +0200

    return to compass original settings

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "./source/css"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "./source/images"
-javascripts_dir = "./source/js"
+images_dir = "images"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
--- images/gallery/_gallery.sh
+++ images/gallery/_gallery.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 input=$1
-outdir="source/images/gallery/`echo $1 |sed -e 's/gallerysource\///'`"
+outdir="`echo $1 |sed -e 's/_//'`"
 
 if [ "${input+x}" = x ] && [ -z "$input" ]; then
-	echo "please indicate folder, correct syntax is: ./generate.sh gallerysource/pathoforiginalimages"
+	echo "please indicate folder, correct syntax is: ./generate.sh _pathoforiginalimages"
 	exit 0
 fi
 
@@ -14,14 +14,14 @@ cp $input/*.jpg tmp/
 
 for img in `ls tmp`; do
 		convert tmp/$img tmp/tmp.png
-		composite -gravity SouthEast gallerysource/wm.png tmp/tmp.png tmp/tmp.png
+		composite -gravity SouthEast _wm.png tmp/tmp.png tmp/tmp.png
 		convert tmp/tmp.png \
 			\( +clone  -alpha extract \
 			-draw 'fill black polygon 0,0 0,10 10,0 fill white circle 10,10 10,0' \
 			\( +clone -flip \) -compose Multiply -composite \
 			\( +clone -flop \) -compose Multiply -composite \
 			\) -alpha off -compose CopyOpacity -composite tmp/tmp.png
-		composite -background white -gravity center tmp/tmp.png gallerysource/background.png tmp/tmp.png
+		composite -background white -gravity center tmp/tmp.png _background.png tmp/tmp.png
 		convert tmp/tmp.png $outdir/$img
 		echo $img
 done
