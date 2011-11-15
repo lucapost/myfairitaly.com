63fba0b6fe6f902fd9d45ab69950c6ca9486efa6
Luca Postregna <luca.postregna@gmail.com>
2011-11-15 18:16:06 +0100

    tregrazienew

--- js/mylibs/slider.js
+++ js/mylibs/slider.js
@@ -2,14 +2,12 @@ $(function(){
 	if(!flux.browser.supportsTransitions)
 		alert("Flux Slider requires a browser that supports CSS3 transitions, please update to Firefox5+ or Chrome12+");
 	window.slider = new flux.slider('#slider', {
-		transitions: [ 'blocks2' ],
         	pagination: true,
                 autoplay: true
 	});
 	window.slider = new flux.slider('#sliderhome', {
-		transitions: [ 'blocks2' ],
         	pagination: false,
+		transitions: [ 'zip' ],
                 autoplay: true
 	});
 });
-
